09cf6891dfef5e6f7902302907550f8542bb1fc1
Michelle Appiah <michappiah67@gmail.com>
2023-11-14 20:06:40 +0000

    setting up

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-const express = require('express');
-const authRoutes = require('./routes/authRoutes');
-const app = express()
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-app.set('view engine', 'ejs');
+const mongoDb = "mongodb+srv://michelleAppiah:3zIip7KrORJbZgpb@cluster0.6ba21px.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
-//set up routes
-app.use('/auth',authRoutes);
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+  })
+);
 
-//home route
-app.get('/', (req,res) => {
-    res.render('home');
-});
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
 
-app.listen(3000, () => {
-    console.log('app listening to port 3000')
-});
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
